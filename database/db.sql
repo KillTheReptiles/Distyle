f43f31c66864611a6dda002caf2811f18cb8b57d
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2021-06-09 18:28:00 -0500

    First commit

---
+++ database/db.sql
@@ -0,0 +1,48 @@
+USE db_links;
+
+-- TABLE USER
+-- all pasword wil be encrypted using SHA1
+CREATE TABLE users (
+  id INT(11) NOT NULL,
+  email VARCHAR(150) NOT NULL,
+  username VARCHAR(16) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  fullname VARCHAR(100) NOT NULL,
+  role VARCHAR(20),
+  logo VARCHAR(100)
+);
+
+ALTER TABLE users
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE users
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
+
+DESCRIBE users;
+
+SELECT * FROM users;
+
+-- LINKS TABLE
+CREATE TABLE links (
+  id INT(11) NOT NULL,
+  title VARCHAR(150) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INT(11),
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE links
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'andresdiaz123';
+
+SELECT * FROM links;
+SELECT * FROM users;
