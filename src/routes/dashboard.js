0e1ade0c246fc854a5aca030b0b0d3225c7840f1
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2021-06-11 22:42:33 -0500

    table users

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 const pool = require('../database');
 const {isLoggedIn, isNotLoggedIn} = require('../lib/auth');
 
-router.get('/dashboard', isLoggedIn, async(req, res) => {
-    const users = await pool.query('SELECT * FROM users');// It send to the list and create an array with the links
-    res.render('dashboard/users/list',{users});
+router.get('/dashboard/users', isLoggedIn, async(req, res) => {
+    const users = await pool.query('SELECT * FROM users');
+    console.log(users)// It send to the list and create an array with the links
+    res.render('dashboard/users/list',{ users: users});
 });
 
-router.get('/dashboard/edit/:id',isLoggedIn, async (req, res) => {
+router.get('/dashboard/users/edit/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const users = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
     res.render('./dashboard/users/edit', { user: users[0] });
 });
 
-router.post('/dashboard/edit/:id', isLoggedIn, async (req, res) => {
+router.post('/dashboard/users/edit/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const { fullname, username, email, role, status } = req.body;
     const editUser = {
